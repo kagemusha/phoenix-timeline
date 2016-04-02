25cb1b82fc9e7851c7fa19497867fd002d9c3eeb
kagemusha <mmadrid99@gmail.com>
2016-04-02 10:50:07 -0400

    add creator, player to game on creation

--- web/controllers/api/game_controller.ex
+++ web/controllers/api/game_controller.ex
@@ -2,6 +2,7 @@ defmodule PhoenixTimeline.Api.GameController do
   use PhoenixTimeline.Web, :controller
 
   alias PhoenixTimeline.Game
+  alias PhoenixTimeline.Player
 
   #plug :scrub_params, "game" when action in [:create, :update]
 
@@ -15,11 +16,22 @@ defmodule PhoenixTimeline.Api.GameController do
     render(conn, data: game)
   end
 
-  def create(conn, %{"data" => %{"attributes" => attrs}}) do
-    changeset = Game.changeset(%Game{}, attrs)
+  def create(conn, %{
+      "data" => %{"attributes" => %{"code" => code},
+      "creator"=> %{"data"=> %{"attributes" => creator_attrs}}}
+   }) do
+    creator_changes = Player.changeset(%Player{}, creator_attrs)
 
-    case Repo.insert(changeset) do
-      {:ok, game} ->
+    case Repo.insert(creator_changes) do
+      {:ok, creator} ->
+        game_params = %{code: code, status: "not_started"}
+        # this really should be atomic with game creation, but waiting
+        # on ecto 2.0 where better assoc support expected
+        game_changes = Ecto.build_assoc(creator, :created_game, game_params)
+        game = Repo.insert!(game_changes)
+        add_creator_as_player = Ecto.build_assoc(game, :players, Map.from_struct(creator))
+        game = Repo.update!(add_creator_as_player)
+
         conn
         |> put_status(:created)
         |> render(:show, data: game)
