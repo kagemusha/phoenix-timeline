bc8421b802a75060425997c70327327d1723fac7
kagemusha <mmadrid99@gmail.com>
2016-04-02 10:16:58 -0400

    add reload to .iex and User => Player

--- .iex.exs
+++ .iex.exs
@@ -1,6 +1,14 @@
 import Ecto.Queryable
 alias PhoenixTimeline.Repo
 alias PhoenixTimeline.Game
-alias PhoenxiTimeline.Card
-alias PhoenxiTimeline.User
+alias PhoenixTimeline.Card
+alias PhoenixTimeline.Player
 
+defmodule R do
+  def reload! do
+    Mix.Task.reenable "compile.elixir"
+    Application.stop(Mix.Project.config[:app])
+    Mix.Task.run "compile.elixir"
+    Application.start(Mix.Project.config[:app], :permanent)
+  end
+end
