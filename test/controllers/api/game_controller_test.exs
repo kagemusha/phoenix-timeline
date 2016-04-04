2f30b53f5651952193b058231b58057dbe15d3bd
kagemusha <mmadrid99@gmail.com>
2016-04-04 19:49:27 -0400

    WIP first attempt at game controller tests
    not working

--- test/controllers/api/game_controller_test.exs
+++ test/controllers/api/game_controller_test.exs
@@ -1,59 +1,50 @@
 defmodule PhoenixTimeline.Api.GameControllerTest do
   use PhoenixTimeline.ConnCase
 
-  alias PhoenixTimeline.Api.Game
-  @valid_attrs %{}
+  alias PhoenixTimeline.Game
+  @valid_attrs %{"attributes" => %{"code" => "guma"},
+      "creator"=> %{"data"=> %{"attributes" => %{"name"=>"yolo"}}}}
   @invalid_attrs %{}
 
   setup %{conn: conn} do
-    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+   conn =
+        conn
+        |> put_req_header("accept", "application/vnd.api+json")
+        |> put_req_header("content-type", "application/vnd.api+json")
+
+    {:ok, conn: conn}
   end
 
-  test "lists all entries on index", %{conn: conn} do
-    conn = get conn, game_path(conn, :index)
-    assert json_response(conn, 200)["data"] == []
-  end
-
-  test "shows chosen resource", %{conn: conn} do
-    game = Repo.insert! %Game{}
-    conn = get conn, game_path(conn, :show, game)
-    assert json_response(conn, 200)["data"] == %{"id" => game.id}
-  end
-
-  test "does not show resource and instead throw error when id is nonexistent", %{conn: conn} do
-    assert_error_sent 404, fn ->
-      get conn, game_path(conn, :show, -1)
-    end
-  end
+#  test "join game", %{conn: conn} do
+#    assert_error_sent 404, fn ->
+#      get conn, game_path(conn, :show, -1)
+#    end
+#  end
 
   test "creates and renders resource when data is valid", %{conn: conn} do
-    conn = post conn, game_path(conn, :create), game: @valid_attrs
+    IO.inspect "game_path: #{game_path(conn, :create)}"
+    params = Poison.encode!(%{data: @valid_attrs})
+    conn = post conn, game_path(conn, :create), params
     assert json_response(conn, 201)["data"]["id"]
     assert Repo.get_by(Game, @valid_attrs)
   end
 
-  test "does not create resource and renders errors when data is invalid", %{conn: conn} do
-    conn = post conn, game_path(conn, :create), game: @invalid_attrs
-    assert json_response(conn, 422)["errors"] != %{}
+#  test "does not create resource and renders errors when data is invalid", %{conn: conn} do
+#    conn = post conn, game_path(conn, :create), data: @invalid_attrs
+#    assert json_response(conn, 422)["errors"] != %{}
+#  end
+#
+#
+#  test "deletes chosen resource", %{conn: conn} do
+#    game = Repo.insert! %Game{}
+#    conn = delete conn, game_path(conn, :delete, game)
+#    assert response(conn, 204)
+#    refute Repo.get(Game, game.id)
+#  end
+  defp create_game_json(player_name, game_code) do
+
+  @valid_attrs %{"attributes" => %{"code" => "guma"},
+      "creator"=> %{"data"=> %{"attributes" => %{"name"=>"yolo"}}}}
   end
 
-  test "updates and renders chosen resource when data is valid", %{conn: conn} do
-    game = Repo.insert! %Game{}
-    conn = put conn, game_path(conn, :update, game), game: @valid_attrs
-    assert json_response(conn, 200)["data"]["id"]
-    assert Repo.get_by(Game, @valid_attrs)
-  end
-
-  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
-    game = Repo.insert! %Game{}
-    conn = put conn, game_path(conn, :update, game), game: @invalid_attrs
-    assert json_response(conn, 422)["errors"] != %{}
-  end
-
-  test "deletes chosen resource", %{conn: conn} do
-    game = Repo.insert! %Game{}
-    conn = delete conn, game_path(conn, :delete, game)
-    assert response(conn, 204)
-    refute Repo.get(Game, game.id)
-  end
 end
