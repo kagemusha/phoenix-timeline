3291403ab56c381240dc9c0ffe434a7289569bc1
kagemusha <mmadrid99@gmail.com>
2016-06-12 19:23:48 -0400

    beginning of validations

--- web/controllers/api/player_controller.ex
+++ web/controllers/api/player_controller.ex
@@ -15,16 +15,33 @@ defmodule PhoenixTimeline.Api.PlayerController do
   def create(conn, %{ "player" => %{"name" => name, "game_code" => game_code} }) do
 
     game = Repo.get_by Game, code: game_code
-    case Player.create(conn, game, name) do
-      {:ok, player} ->
-        player = Repo.preload player, :game
-        conn
-        |> put_status(:created)
-        |> render("player_with_game.json", %{player: player})
-      {:error, _changeset} ->
-        IO.puts "ERROR SAVING PLAYER"
-    end
 
+    cond do
+
+      !game ->
+        message = "game.not.found"
+        conn
+        |> put_resp_content_type("application/json")
+        |> send_resp(409, message)
+
+      game.status != "waiting-to-start" ->
+        conn
+        |> put_resp_content_type("application/json")
+        |> send_resp(409, "game.already.started")
+
+      true ->
+        #if has same name
+        #if has no name
+        case Player.create(conn, game, name) do
+          {:ok, player} ->
+            player = Repo.preload player, :game
+            conn
+            |> put_status(:created)
+            |> render("player_with_game.json", %{player: player})
+          {:error, _changeset} ->
+            IO.puts "ERROR SAVING PLAYER"
+        end
+    end
   end
 
 end
