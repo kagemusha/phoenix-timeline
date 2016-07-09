f762614ad4f89a98410df17ccd522fc66b8b6686
kagemusha <mmadrid99@gmail.com>
2016-07-09 19:48:22 -0400

    move import_config to bottom of file

--- config/config.exs
+++ config/config.exs
@@ -22,12 +22,12 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
-
 # Configure phoenix generators
 config :phoenix, :generators,
   migration: true,
   binary_id: false
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
 
